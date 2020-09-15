459c3b6ef0bac6d310cfa62fd941736a723ab60c
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-09-15 20:29:38 +0600

    Ema-Jhon done

---
+++ src/cart/Cart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    //const total = cart.reduce( (total, prd ) => total + prd.price, 0);
+    let total = 0;
+    for(let i = 0; i < cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+    let shipping = 0;
+    if(total > 35){
+        shipping = 0;
+    }
+    else if(total > 15){
+        shipping = 4.99;
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+    return (
+        <div>
+            <h2>Order Summary </h2>
+            <h5>Items Ordered: {cart.length}</h5>
+    <p>Product Price: {formatNumber(total)}</p>
+    <p><small>Shipping: {formatNumber(shipping)}</small></p>
+    <p><small>Tax + VAT: {formatNumber(tax)}</small></p>
+            <p>Total Price: {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Shop.css';
+import fakeData from '../../fakeData';
+import { useState } from 'react';
+import Product from '../Products/Product';
+import Cart from '../../cart/Cart';
+
+const Shop = () => {
+    //console.log(fakeData);
+    const productNum = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(productNum);
+    const [cart, setCart] = useState([]);
+
+    const handleCart = (product) => {
+      console.log('Product Added', product);
+      const newCart = [...cart, product];
+      setCart(newCart);
+    };
+    
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <h2>Products: {products.length}</h2>
+                <ul>
+                    {
+                        products.map(pd => <Product product={pd} handleCart={handleCart}></Product>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
